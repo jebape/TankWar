f35842821784b29e34f4402e1e5fe25977b8a78c
jebape <jesusballesterosperal@gmail.com>
2019-03-27 23:09:05 +0100

    updated TankPlayerController

--- TankWar/Source/TankWar/Private/TankPlayerController.cpp
+++ TankWar/Source/TankWar/Private/TankPlayerController.cpp
@@ -3,6 +3,18 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto controlledTank = GetControlledTank();
+	if (!controlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("There's no controlled tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Player controller possesing: %s"), *(controlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller BeginPlay"));
+}
 
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
--- TankWar/Source/TankWar/Public/TankPlayerController.h
+++ TankWar/Source/TankWar/Public/TankPlayerController.h
@@ -17,6 +17,6 @@ class TANKWAR_API ATankPlayerController : public APlayerController
 	
 public:	
 	ATank* GetControlledTank() const;
-	
+	void BeginPlay() override;
 	
 };
